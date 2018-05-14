66333ae97e7e8890025ef741e788d42a6afee14c
Sachin Sharma <amrahs.nihcas@gmail.com>
2018-05-15 04:52:51 +0530

    added some styles

--- yelp-clone/v1/app.js
+++ yelp-clone/v1/app.js
@@ -5,6 +5,9 @@ var bodyParser = require("body-parser");
 var campgrounds = [
     {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8515/8365336428_abb241726d.jpg"},
     {name: "Granite Hill", image: "https://farm2.staticflickr.com/1075/1132747626_f7adec63dd.jpg"},
+    {name: "Yellowstone Park", image: "https://pixabay.com/get/e830b9062ef5073ed1584d05fb1d4e97e07ee3d21cac104497f6c37ea2e5bdb1_340.jpg"},
+    {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8515/8365336428_abb241726d.jpg"},
+    {name: "Granite Hill", image: "https://farm2.staticflickr.com/1075/1132747626_f7adec63dd.jpg"},
     {name: "Yellowstone Park", image: "https://pixabay.com/get/e830b9062ef5073ed1584d05fb1d4e97e07ee3d21cac104497f6c37ea2e5bdb1_340.jpg"}
 ]
 
