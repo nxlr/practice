341bafc3709d2f8d3c307ec84d8a2fcba0e92780
Sachin Sharma <amrahs.nihcas@gmail.com>
2018-05-17 15:47:46 +0530

    Associating comments and campgrounds with author

---
+++ yelp-clone/v8/app.js
@@ -0,0 +1,52 @@
+var express     = require("express"),
+    app         = express(),
+    bodyParser  = require("body-parser"),
+    mongoose    = require("mongoose"),
+    passport    = require("passport"),
+    localStrategy = require("passport-local"),
+    Campground  = require("./models/campground"),
+    seedDB      = require("./seeds"),
+    User        = require("./models/user"),
+    Comment     = require("./models/comment");
+
+// REQUIRING ROUTES
+var commentRoutes       = require("./routes/comments"),
+    campgroundRoutes    = require("./routes/campgrounds"),
+    authRoutes          = require('./routes/index');
+
+mongoose.connect("mongodb://localhost/yelp_camp_v6");
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
+// Seed the databse
+// seedDB();
+
+// PASSPORT CONFIGURATION
+app.use(require("express-session")({
+    secret: "My fingers are gonna break",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new localStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next){
+    res.locals.currentUser = req.user;
+    next();
+});
+
+app.use("/",authRoutes);
+app.use("/campgrounds",campgroundRoutes);
+app.use("/campgrounds/:id/comments",commentRoutes);
+
+process.env.PORT = "3000";
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Yelp-Clone Server started.");
+});
+
---
+++ yelp-clone/v8/models/campground.js
@@ -0,0 +1,24 @@
+var mongoose = require("mongoose");
+
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    description: String,
+    author: {
+        id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        },
+        username: String
+    },
+    comments: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Comment"
+        }
+    ]
+});
+
+// Create a collection called campgrounds, 
+// And a model Campground to access collection methods.
+module.exports = mongoose.model("campground", campgroundSchema); 
