2f5efd950d8529ba90d7145e2f011f4a7e4972e9
Sachin Sharma <amrahs.nihcas@gmail.com>
2018-05-18 08:54:42 +0530

    Adding authorization for Editing and Deleting Comments

--- yelp-clone/v10/routes/comments.js
+++ yelp-clone/v10/routes/comments.js
@@ -45,11 +45,69 @@ router.post("/", isLoggedIn, function(req, res){
     
 });
 
+// EDIT
+router.get("/:comment_id/edit", checkCommentOwnership, function(req, res){
+    Comment.findById(req.params.comment_id, function(err, foundComment){
+        if(err){
+            res.redirect("back");
+        } else {
+            res.render("comments/edit", {campground_id: req.params.id, comment: foundComment});
+        } 
+    });
+});
+
+// UPDATE
+router.put("/:comment_id", checkCommentOwnership, function(req, res){
+    // find and update the correct campground
+    Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment){
+        if(err){
+            res.redirect("back");
+        } else{
+            // redirect to edited page
+            res.redirect("/campgrounds/" + req.params.id); // or use updatedCampground._id
+        }
+    });
+});
+
+// DESTROY
+router.delete("/:comment_id", checkCommentOwnership, function(req, res){
+    // find and update the correct campground
+    Comment.findByIdAndRemove(req.params.comment_id, function(err){
+        if(err){
+            res.redirect("back");
+        } else{
+            // redirect to edited page
+            res.redirect("/campgrounds/" + req.params.id); // or use updatedCampground._id
+        }
+    });
+});
+
+// middleware
 function isLoggedIn(req, res, next){
     if(req.isAuthenticated()){
         return next();
     } 
     res.redirect("/login");
+}
+
+function checkCommentOwnership(req, res, next){
+    if(req.isAuthenticated()){
+        Comment.findById(req.params.comment_id, function(err, foundComment){
+            if(err){
+                res.redirect("back");
+            } else {
+                // does the user own the campground
+                // console.log(foundCampground.author.id)
+                if(foundComment.author.id.equals(req.user._id)){
+                    next();
+                } else {
+                    res.redirect("back");
+                }
+            }
+        });
+    } else {
+        res.redirect("back");
+    }
 }
 
 module.exports = router;
